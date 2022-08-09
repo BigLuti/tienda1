bc91d456351d0dfe3c96ab8bd4ce9ea2bb3745f2
Luti <92194635+BigLuti@users.noreply.github.com>
2022-08-08 22:46:14 -0600

    avance tienda

---
+++ TiendaLA/src/main/java/com/tienda/WebConfig.java
@@ -0,0 +1,41 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.tienda;
+
+import java.util.Locale;
+import lombok.experimental.var;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import org.springframework.web.servlet.i18n.LocaleChangeInterceptor;
+import org.springframework.web.servlet.i18n.SessionLocaleResolver;
+
+
+
+@Configuration
+/* Las configurasciones */
+public class WebConfig implements WebMvcConfigurer {
+     @Bean
+    public SessionLocaleResolver localeResolver(){
+        var slr = new SessionLocaleResolver();
+        slr.setDefaultLocale(new Locale("en"));
+        return slr;
+    }
+    
+    @Bean
+    public LocaleChangeInterceptor localeChangeInterceptor(){
+        var lci = new LocaleChangeInterceptor();
+        lci.setParamName("lang");
+        return lci;
+    }
+    
+    @Override
+    public void addInterceptors(InterceptorRegistry registro){
+        registro.addInterceptor(localeChangeInterceptor());
+    } 
+    
+}
